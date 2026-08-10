d96a833646d5f16fa39bd06fde7e7e7adad28a7f
Hugo Almeida <0911616457.za@gmail.com>
2026-08-10 05:09:11 +0300

    chore:logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,38 +1,26 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file='app.log', max_bytes=5 * 1024 * 1024, backup_count=3):
-    """
-    Sets up a logger that writes log messages to a file with rotation.
-    The log file will rotate when it exceeds max_bytes, keeping a number of backup files.
-    """
-    # Create a logger
-    logger = logging.getLogger('my_logger')
-    logger.setLevel(logging.DEBUG)
+# Setting up the logger
 
-    # Create a file handler that rotates the log files
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
+    """
+    Configures a logger with a rotating file handler.
+    :param log_file: Path to the log file
+    :param max_bytes: Max size of log file before rotation
+    :param backup_count: Number of backup files to keep
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  # Set the logging level
+
+    # Create a rotating file handler
     handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
-    # Create a console handler for output to terminal
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-
-    # Create a formatter and set it for both handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
-
-    # Add the handlers to the logger
     logger.addHandler(handler)
-    logger.addHandler(console_handler)
-
     return logger
 
-# Example usage: Uncomment to test
-# if __name__ == '__main__':
-#     log = setup_logger()
-#     log.debug('This is a debug message')
-#     log.info('This is an info message')
-#     log.warning('This is a warning message')
-#     log.error('This is an error message')
-#     log.critical('This is a critical message')
+# Usage example
+if __name__ == '__main__':
+    logger = setup_logger()
+    logger.info('Logger is set up and ready to use.')
